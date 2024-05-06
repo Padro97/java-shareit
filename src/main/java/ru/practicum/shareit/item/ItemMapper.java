fa75b0c49f2694fdadc7f66a70d94500c74152dc
Nikita <np12231223@yandex.ru>
2024-05-06 19:58:44 +0300

    fix checkstyle

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareit.item;
 
-import org.mapstruct.Context;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import ru.practicum.shareit.user.UserMapper;
 
 @Mapper(componentModel = "spring", uses = UserMapper.class)
