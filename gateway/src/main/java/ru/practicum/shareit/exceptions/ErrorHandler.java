54431d366edfd7abba8e18c1dc127e5de40cec3b
Nikita <np12231223@yandex.ru>
2024-06-21 20:53:50 +0300

    Вернул все в изначальное состояние

--- gateway/src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -1,37 +1,16 @@
 package ru.practicum.shareit.exceptions;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Map;
-
 @RestControllerAdvice
 public class ErrorHandler {
-    private static final Logger log = LoggerFactory.getLogger(ErrorHandler.class);
-
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    @ExceptionHandler({MethodArgumentNotValidException.class, ValidationException.class, IllegalArgumentException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidationException(final MethodArgumentNotValidException e) {
-        log.error("Validation error: {}", e.getMessage(), e);
-        return Map.of("Ошибка валидации", e.getMessage());
-    }
-
-    @ExceptionHandler(value = IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleIllegalArgument(final IllegalArgumentException e) {
-        log.error("Illegal argument: {}", e.getMessage(), e);
-        return Map.of("error", e.getMessage());
-    }
-
-    @ExceptionHandler(value = IllegalStateException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleIllegalState(final IllegalStateException e) {
-        log.error("Illegal state: {}", e.getMessage(), e);
-        return Map.of("error", e.getMessage());
+    public ErrorResponse validateException(final RuntimeException e) {
+        return new ErrorResponse(e.getMessage());
     }
 }
